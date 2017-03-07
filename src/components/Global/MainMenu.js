2b249455bfb3998b653b6c63d23e2df9920fc8ca
Lucas Cherkewski <lucas@ecustom.ca>
2017-03-06 22:05:34 -0500

    Markup for nav menu, very preliminary styles

--- src/components/Global/MainMenu.js
+++ src/components/Global/MainMenu.js
@@ -3,8 +3,25 @@ const React = require('react');
 class MainMenu extends React.Component {
     render() {
         return (
-            <div>
-
+            <div className="flex">
+                <p className="b f4 pv3 ph4 ma0 lh-solid dib ba b--near-white fl">SpotHire</p>
+                <nav className="fl flex-auto flex">
+                    <div className="w-20 ba b--near-white justify-center inline-flex">
+                        <a className="self-center" href="#">Me</a>
+                    </div>
+                    <div className="w-20 ba b--near-white justify-center inline-flex">
+                        <a className="self-center" href="#">Newsfeed</a>
+                    </div>
+                    <div className="w-20 ba b--near-white justify-center inline-flex">
+                        <a className="self-center" href="#">People</a>
+                    </div>
+                    <div className="w-20 ba b--near-white justify-center inline-flex">
+                        <a className="self-center" href="#">Schedule</a>
+                    </div>
+                    <div className="w-20 ba b--near-white justify-center inline-flex">
+                        <a className="self-center" href="#">Timesheet</a>
+                    </div>
+                </nav>
             </div>
         )
     }
