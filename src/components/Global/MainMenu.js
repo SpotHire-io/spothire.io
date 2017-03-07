9f4c8ce286884b95eb3cd231be5723f08b0c24e0
Lucas Cherkewski <lucas@ecustom.ca>
2017-03-07 13:51:57 -0500

    some tweaks to menu items

--- src/components/Global/MainMenu.js
+++ src/components/Global/MainMenu.js
@@ -4,15 +4,15 @@ class MainMenu extends React.Component {
     renderNavLink(linkText) {
         return (
             <div className="w-20 inline-flex items-start tc">
-                <a className="f6 pv3 w-100 bt bb b--near-white bg-dark-blue white link dim" href="#">{linkText}</a>
+                <a className="f6 pv3 w-100 bt bb bw1 b--near-white sh-bg-sea-blue white link dim" href="#">{linkText}</a>
             </div>
         )
     }
 
     render() {
         return (
-            <div className="flex">
-                <p className="b bg-blue white f4 pv3 ph4 ma0 lh-solid ba bw1 b--near-white">SpotHire</p>
+            <div className="flex avenir">
+                <p className="b sh-bg-true-blue white f4 pv3 ph4 ma0 lh-solid ba bw1 b--near-white">SpotHire</p>
                 <nav className="flex-auto flex">
                     {
                         [
