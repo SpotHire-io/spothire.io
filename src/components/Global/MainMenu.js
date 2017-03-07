aa8a4d2c1ee644594dc2a5b57c8dc93d5aa4d384
Lucas Cherkewski <lucas@ecustom.ca>
2017-03-07 13:58:20 -0500

    conditional styles for last menuItem

--- src/components/Global/MainMenu.js
+++ src/components/Global/MainMenu.js
@@ -1,27 +1,36 @@
-const React = require('react');
+const React      = require('react');
+const classNames = require('classnames');
 
 class MainMenu extends React.Component {
-    renderNavLink(linkText) {
+    renderNavLink(linkText, isLast) {
+        const aClasses = classNames({
+            'f6 pv3 w-100 bt bb bw1 b--near-white sh-bg-sea-blue white link dim': true,
+            'br br--dotted': ! isLast,
+            'br': isLast,
+        });
+
         return (
             <div className="w-20 inline-flex items-start tc">
-                <a className="f6 pv3 w-100 bt bb br bw1 b--near-white sh-bg-sea-blue white link dim" href="#">{linkText}</a>
+                <a className={aClasses} href="#">{linkText}</a>
             </div>
         )
     }
 
     render() {
+        const menuItems = [
+            'Dashboard',
+            'Newsfeed',
+            'People',
+            'Schedule',
+            'Timesheet',
+        ];
+
         return (
             <div className="flex avenir">
                 <p className="b sh-bg-true-blue white f4 pv3 ph4 ma0 lh-solid ba bw1 b--near-white">SpotHire</p>
                 <nav className="flex-auto flex">
                     {
-                        [
-                            'Dashboard',
-                            'Newsfeed',
-                            'People',
-                            'Schedule',
-                            'Timesheet',
-                        ].map((text) => this.renderNavLink(text))
+                        menuItems.map((text, index) => this.renderNavLink(text, menuItems.length === index + 1))
                     }
                 </nav>
             </div>
