5948dfa990b6650ac57241ef50c55a4d209b246d
Lucas Cherkewski <lucas@ecustom.ca>
2017-03-06 22:13:12 -0500

    Switch to using a method to handle link rendering

--- src/components/Global/MainMenu.js
+++ src/components/Global/MainMenu.js
@@ -1,26 +1,28 @@
 const React = require('react');
 
 class MainMenu extends React.Component {
+    renderNavLink(linkText) {
+        return (
+            <div className="w-20 ba b--near-white justify-center inline-flex">
+                <a className="self-center" href="#">{linkText}</a>
+            </div>
+        )
+    }
+
     render() {
         return (
             <div className="flex">
                 <p className="b f4 pv3 ph4 ma0 lh-solid dib ba b--near-white fl">SpotHire</p>
                 <nav className="fl flex-auto flex">
-                    <div className="w-20 ba b--near-white justify-center inline-flex">
-                        <a className="self-center" href="#">Me</a>
-                    </div>
-                    <div className="w-20 ba b--near-white justify-center inline-flex">
-                        <a className="self-center" href="#">Newsfeed</a>
-                    </div>
-                    <div className="w-20 ba b--near-white justify-center inline-flex">
-                        <a className="self-center" href="#">People</a>
-                    </div>
-                    <div className="w-20 ba b--near-white justify-center inline-flex">
-                        <a className="self-center" href="#">Schedule</a>
-                    </div>
-                    <div className="w-20 ba b--near-white justify-center inline-flex">
-                        <a className="self-center" href="#">Timesheet</a>
-                    </div>
+                    {
+                        [
+                            'Me',
+                            'Newsfeed',
+                            'People',
+                            'Schedule',
+                            'Timesheet',
+                        ].map((text) => this.renderNavLink(text))
+                    }
                 </nav>
             </div>
         )
