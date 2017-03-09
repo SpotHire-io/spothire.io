4faf9ce72b98df06e7c4febc4a14598f4b54be5c
Lucas Cherkewski <lucas@ecustom.ca>
2017-03-08 20:40:51 -0500

    start to buttons

---
+++ src/components/Buttons/BasicButton.js
@@ -0,0 +1,17 @@
+const React      = require('react');
+const classNames = require('classnames');
+
+const BasicButton = ({ children, className, onClick }) => {
+    let buttonClasses = classNames({
+        'button': true,
+        [className]: true
+    });
+
+    return <button onClick={onClick} className={buttonClasses}>{children}</button>;
+};
+
+BasicButton.defaultProps = {
+    className: ''
+};
+
+module.exports = BasicButton;
--- src/stories/index.js
+++ src/stories/index.js
@@ -4,11 +4,20 @@ const WithNotes = require('@kadira/storybook-addon-notes').WithNotes;
 
 const MainMenu = require('../components/Global/MainMenu');
 
+const BasicButton = require('../components/Buttons/BasicButton');
+
 import '../../public/css/style.css';
 
 storiesOf('Global', module)
     .add('MainMenu', () => (
         <WithNotes>
             <MainMenu/>
+        </WithNotes>
+    ));
+
+storiesOf('Buttons', module)
+    .add('BasicButton:Neutral', () => (
+        <WithNotes>
+            <BasicButton className="button--neutral">Add Talent</BasicButton>
         </WithNotes>
     ));
