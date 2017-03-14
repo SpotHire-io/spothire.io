cfccd03d4b62fd286df4b495b5455f624b489da0
Lucas Cherkewski <lucas@ecustom.ca>
2017-03-13 22:08:16 -0400

    output + format post content

--- src/components/Newsfeed/PostItem.js
+++ src/components/Newsfeed/PostItem.js
@@ -1,15 +1,25 @@
 const React      = require('react');
 const classNames = require('classnames');
+const moment     = require('moment');
 
 const PostItem = ({ post, className }) => {
     let wrapperClasses = classNames({
-        '': true,
+        'pt-sans': true,
         [className]: true
     });
 
     return (
         <div className={wrapperClasses}>
-            <h2>{post.title}</h2>
+            <div className="bg-white ba b--black-20 pa3">
+                <h2 className="mv0 lh-title">{post.title}</h2>
+
+                <div className="mt3 georgia">
+                    {post.content}
+                </div>
+            </div>
+            <div className="bg-white-10 ba bt-0 b--black-10 ph3 pv2 f6">
+                {moment(post.date).format("MMMM Do, h:mm a")}
+            </div>
         </div>
     );
 };
--- src/stories/index.js
+++ src/stories/index.js
@@ -64,14 +64,22 @@ storiesOf('Buttons', module)
 storiesOf('Newsfeed', module)
     .add('PostItem', () => (
         <WithNotes>
-            <PostItem
-                post={{
-                    id: 1,
-                    title: "A very cool post",
-                    content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum reprehenderit rerum sit vitae! Adipisci eum ipsam libero nam nesciunt quae, tenetur! Fugiat inventore iure libero quis voluptate! Accusantium alias debitis deserunt doloremque est et eum ex hic illum impedit ipsum minima minus nihil, non odit optio perspiciatis possimus provident quae quaerat quia quidem quo quod similique sit. A ad aliquid animi corporis dignissimos eos est ex, excepturi fuga, id inventore itaque libero maiores nisi omnis, quidem quo quod ratione soluta sunt totam veritatis voluptate. Ad consequatur debitis dolorem eos, id iusto laboriosam natus nulla officiis porro quis quod repellat, suscipit temporibus totam? Accusamus at atque autem beatae ducimus fugiat, inventore numquam officia qui sapiente sequi tenetur, unde. Animi aperiam assumenda debitis iusto nemo nisi obcaecati praesentium, repellendus rerum? Accusamus, asperiores beatae dicta distinctio maiores minus non optio possimus quas quidem repellat sint voluptatum? Assumenda distinctio, dolorem dolorum fugiat quasi saepe.",
-                    date: "2017-03-13T12:00:00-04:00",
-                    responseRequired: true
-                }}
-            />
+            <div className="pa4 bg-near-white">
+                <PostItem
+                    post={{
+                        id: 1,
+                        title: "A very cool post",
+                        content: (
+                            <div>
+                                <p className="mt0">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos ducimus fugiat fugit, minus modi qui quod ratione repellat ut vero?</p>
+
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet cumque enim, illo odio temporibus vitae voluptatem! Adipisci amet architecto at cum doloremque dolorum ducimus eveniet ex, facere iure libero molestiae non numquam quam rem sit ut vel? Animi aperiam assumenda consectetur dolore dolorem eaque ex exercitationem facilis labore natus obcaecati quisquam quos recusandae rem sint totam vero, voluptatum? Accusamus animi aperiam aspernatur at distinctio dolor ducimus eveniet facere, fugiat incidunt ipsa magnam maxime molestiae molestias, officiis pariatur perspiciatis praesentium, quisquam rem saepe sunt velit voluptas voluptatibus. Assumenda odio provident quidem recusandae repellat. Distinctio ducimus facere illum ipsam similique sit voluptatem.</p>
+                            </div>
+                        ),
+                        date: "2017-03-13T12:00:00-04:00",
+                        responseRequired: true
+                    }}
+                />
+            </div>
         </WithNotes>
     ));
