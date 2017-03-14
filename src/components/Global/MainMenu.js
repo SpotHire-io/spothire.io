7af5d97cfc569b769bb0806a68e517b8e328d394
Lucas Cherkewski <lucas@ecustom.ca>
2017-03-13 21:41:45 -0400

    refactor menu a bit (still a long way to go)

--- src/components/Global/MainMenu.js
+++ src/components/Global/MainMenu.js
@@ -4,15 +4,13 @@ const classNames = require('classnames');
 class MainMenu extends React.Component {
     renderNavLink(linkText, isLast) {
         const aClasses = classNames({
-            'f6 pv3 w-100 bt bb bw1 b--near-white sh-bg-sea-blue-gradient white link dim': true,
+            'w-20 tc f6 w-100 bt bb bw1 b--deep-koamaru bg-blue-yonder washed-yellow link dim': true,
             'br br--dotted': ! isLast,
             'br': isLast,
         });
 
         return (
-            <div className="w-20 inline-flex items-start tc">
-                <a className={aClasses} href="#">{linkText}</a>
-            </div>
+            <a className={aClasses} href="#">{linkText}</a>
         )
     }
 
@@ -26,9 +24,9 @@ class MainMenu extends React.Component {
         ];
 
         return (
-            <div className="flex avenir">
-                <p className="b sh-bg-true-blue white f4 pv3 ph4 ma0 lh-solid ba bw1 b--near-white">SpotHire</p>
-                <nav className="flex-auto flex">
+            <div className="flex pt-sans">
+                <p className="b bg-blue-yonder washed-yellow f4 pv3 ph4 ma0 lh-solid ba bw1 b--deep-koamaru">SpotHire</p>
+                <nav className="flex-auto flex items-center">
                     {
                         menuItems.map((text, index) => this.renderNavLink(text, menuItems.length === index + 1))
                     }
