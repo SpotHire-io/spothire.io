8e4068c07535449a919dc4d78e98cd4f0ebae497
Lucas Cherkewski <lucas@ecustom.ca>
2017-03-16 20:31:40 -0400

    oops, revert that

--- src/components/Newsfeed/Post.js
+++ src/components/Newsfeed/Post.js
@@ -4,42 +4,40 @@ const moment     = require('moment');
 
 const BasicButton = require('../Buttons/BasicButton');
 
-class Post extends React.Component {
-    render() {
-        let wrapperClasses = classNames({
-            'pt-sans': true,
-            [this.props.className]: true
-        });
+const Post = ({ post, className, isOpen }) => {
+    let wrapperClasses = classNames({
+        'pt-sans': true,
+        [className]: true
+    });
 
-        return (
-            <div className={wrapperClasses}>
-                <div className="bg-white ba b--black-20 pa3">
-                    <h2 className="mv0 lh-title">{this.props.post.title}</h2>
+    return (
+        <div className={wrapperClasses}>
+            <div className="bg-white ba b--black-20 pa3">
+                <h2 className="mv0 lh-title">{post.title}</h2>
 
-                    <div className="mt3 georgia">
-                        {this.props.post.content}
-                    </div>
-                </div>
-                <div className="bg-white ba bt-0 b--black-20 flex">
-                    <div className="pa3 br b--black-20 w-50 flex items-center justify-center">
-                        <p className="ma0">Response {this.props.post.isRequired ? 'required' : 'not required'}</p>
-                    </div>
-                    <div className="pa3 w-50">
-                        <textarea className="w-100 b--black-10" rows="3"/>
-
-                        <div className="tr mt2">
-                            <BasicButton className="button--neutral">Reset</BasicButton>
-                            <BasicButton className="button--positive ml2">Submit</BasicButton>
-                        </div>
-                    </div>
-                </div>
-                <div className="bg-white-10 ba bt-0 b--black-10 ph3 pv2 f6">
-                    {moment(this.props.post.date).format("MMMM Do, h:mm a")}
+                <div className="mt3 georgia">
+                    {post.content}
                 </div>
             </div>
-        );
-    }
-}
+            <div className="bg-white ba bt-0 b--black-20 flex">
+                <div className="pa3 br b--black-20 w-50 flex items-center justify-center">
+                    <p className="ma0">Response {post.isRequired ? 'required' : 'not required'}</p>
+                </div>
+                <div className="pa3 w-50">
+                    <textarea className="w-100 b--black-10" rows="3"/>
+
+                    <div className="tr mt2">
+                        <BasicButton className="button--neutral">Reset</BasicButton>
+                        <BasicButton className="button--positive ml2">Submit</BasicButton>
+                    </div>
+                </div>
+            </div>
+            <div className="bg-white-10 ba bt-0 b--black-10 ph3 pv2 f6">
+                {moment(post.date).format("MMMM Do, h:mm a")}
+            </div>
+        </div>
+    );
+};
 
 Post.defaultProps = {
     className: ''
