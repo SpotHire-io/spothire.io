b468df2fdfbcad2f6d5c73968687c4581f1fb202
Lucas Cherkewski <lucas@ecustom.ca>
2017-03-20 22:12:59 -0400

    convert UserList to Table

--- src/components/Miscellaneous/UserList.js
+++ src/components/Miscellaneous/UserList.js
@@ -3,11 +3,21 @@ const classNames = require('classnames');
 
 const userData = require('../../data/users.json');
 
+const Table = require('reactable').Table;
+const Thead = require('reactable').Thead;
+const Th    = require('reactable').Th;
+const Tr    = require('reactable').Tr;
+const Td    = require('reactable').Td;
+
 class UserList extends React.Component {
     constructor() {
         super();
 
         this.renderUserRow = this.renderUserRow.bind(this);
+        this.renderUserCell = this.renderUserCell.bind(this);
+        this.renderHeaderCell = this.renderHeaderCell.bind(this);
+
+        this.commonCellClasses = 'ph2 pv2';
 
         this.state = {};
     }
@@ -18,23 +28,62 @@ class UserList extends React.Component {
         });
 
         return (
-            <li className={userClasses}>
-                {user.firstName + ' ' + user.lastName}
-            </li>
+            <Tr className={userClasses}>
+                {this.renderUserCell('name', user.firstName + ' ' + user.lastName)}
+                {this.renderUserCell('email', user.email)}
+                {this.renderUserCell('phone', user.phone)}
+            </Tr>
+        );
+    }
+
+    renderUserCell(column, value) {
+        const cellClasses = classNames({
+            'bb b--black-20': true,
+            [this.commonCellClasses]: true
+        });
+
+        return (
+            <Td
+                column={column}
+                className={cellClasses}
+            >
+                {value}
+            </Td>
+        );
+    }
+
+    renderHeaderCell(column, value) {
+        const cellClasses = classNames({
+            'tl bg-blue-yonder washed-yellow': true,
+            [this.commonCellClasses]: true
+        });
+
+        return (
+            <Th
+                column={column}
+                className={cellClasses}
+            >
+                {value}
+            </Th>
         );
     }
 
     render() {
         const wrapperClasses = classNames({
-            'bg-white ba b--black-20 br1 sh-shadow-2': true,
+            'bg-white ba b--black-20 br1 sh-shadow-2 app-sans': true,
             [this.props.className]: true
         });
 
         return (
             <div className={wrapperClasses}>
-                <ol className="pa0 list ma0">
+                <Table className="w-100" cellSpacing="0">
+                    <Thead>
+                        {this.renderHeaderCell('name', 'Name')}
+                        {this.renderHeaderCell('email', 'Email')}
+                        {this.renderHeaderCell('phone', 'Phone')}
+                    </Thead>
                     {userData.map((user) => this.renderUserRow(user))}
-                </ol>
+                </Table>
             </div>
         )
     }
