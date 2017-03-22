68054e36620b859d3905d746c84890560d4606e4
Lucas Cherkewski <lucas@ecustom.ca>
2017-03-22 19:47:43 -0400

    refine secondary menu styles a bit

--- src/components/Global/SecondaryMenu.js
+++ src/components/Global/SecondaryMenu.js
@@ -10,13 +10,13 @@ class SecondaryMenu extends React.Component {
 
     renderNavItem(item) {
         return (
-            <a className="link bb bw1 b--red hover-b--white-40" href={item.href}>{item.text}</a>
+            <a className="b near-black hover-gray dib pv3 ph1 mh1 no-underline animate-all bb bw1 b--blue-yonder hover-b--white-40" href={item.href} style={{marginBottom:'-0.125rem'}}>{item.text}</a>
         )
     }
 
     render() {
         return (
-            <nav className="bb bw1 b--blue-yonder ph3" style={{marginBottom: '-0.125rem'}}>
+            <nav className="bb bw1 b--blue-yonder ph3 app-sans">
                 {this.props.items.map((item) => this.renderNavItem(item))}
             </nav>
         );
