c9d28ccf2311b527d9889bec0803474ad7420187
Lucas Cherkewski <lucas@ecustom.ca>
2017-03-27 21:34:48 -0400

    get calendar working

--- src/stories/index.js
+++ src/stories/index.js
@@ -6,6 +6,8 @@ const WithNotes = require('@kadira/storybook-addon-notes').WithNotes;
 const SingleDatePicker = require('react-dates').SingleDatePicker;
 const DateRangePicker  = require('react-dates').DateRangePicker;
 
+const START_DATE = require('react-dates/constants').START_DATE;
+
 const BigCalendar = require('react-big-calendar');
 const moment      = require('moment');
 
@@ -420,6 +422,34 @@ storiesOf('Filters', module)
                 }}
             />
         </WithNotes>
+    ));
+
+const sampleEvents = [
+    {
+        id: 1,
+        start: new Date(2017, 2, 26, 0, 0, 0),
+        end: new Date(2017, 2, 28, 0, 0, 0),
+        title: 'Test',
+        desc: 'A testing description.'
+    }
+];
+
+storiesOf('Scheduling', module)
+    .add('DateRangePicker', () => (
+        <DateRangePicker
+            startDate={moment()}
+            endDate={moment().add(3, 'days')}
+            onDatesChange={({ startDate, endDate }) => console.log({ startDate, endDate })}
+            focusedInput={START_DATE}
+            onFocusChange={focusedInput => console.log({ focusedInput })}
+        />
+    ))
+    .add('Calendar', () => (
+        <div style={{ height: '400px' }}>
+            <BigCalendar
+                events={sampleEvents}
+            />
+        </div>
     ));
 
 storiesOf('Miscellaneous', module)
