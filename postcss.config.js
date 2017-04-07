944ae3bb64f5971127c5b00f18c489ea723a162a
Lucas Cherkewski <lucas@ecustom.ca>
2017-04-07 19:55:22 -0400

    begin separating out vendor/vendor overrides

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     plugins: [
         require('postcss-import')(),
+        require('postcss-extend')(),
         require('postcss-css-variables')(),
         require('autoprefixer')()
     ]
