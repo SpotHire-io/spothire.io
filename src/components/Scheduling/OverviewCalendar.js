662e792794fc39ac2f863392a50e0b0e0392f81a
Lucas Cherkewski <lucas@ecustom.ca>
2017-04-08 21:00:33 -0400

    rejig display format for calendar input

--- src/components/Scheduling/OverviewCalendar.js
+++ src/components/Scheduling/OverviewCalendar.js
@@ -146,6 +146,7 @@ class OverviewCalendar extends React.Component {
                                         isDayBlocked={day => {
 
                                         }}
+                                        displayFormat="MMMM Do, YYYY"
                                     />
                                 </dd>
                             </dl>
