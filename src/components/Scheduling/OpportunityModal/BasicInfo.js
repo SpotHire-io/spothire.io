5ef7da2ad470aee9c4a4b830a7c17b4c7549af43
Lucas Cherkewski <lucas@ecustom.ca>
2017-04-29 12:04:29 -0400

    add a $ prefix to pay rate box

--- src/components/Scheduling/OpportunityModal/BasicInfo.js
+++ src/components/Scheduling/OpportunityModal/BasicInfo.js
@@ -129,7 +129,12 @@ class OpportunityModalBasicInfo extends React.Component {
                     </p>
                     <p className="w-50 mt0">
                         <label className="f6 db" htmlFor="opp_rate">Pay rate</label>
-                        <input className="mt2 w-100" type="number" id="opp_rate" name="opp_rate"/>
+                        <div className="inline-flex w-100 mt2">
+                            <div className="inline-flex self-stretch ph3 bg-near-white ba br-0 b--moon-gray">
+                                <span className="self-center">$</span>
+                            </div>
+                            <input className="flex-auto" type="number" id="opp_rate" name="opp_rate"/>
+                        </div>
                     </p>
                 </div>
 
