b1a7724c1afd654fab0a455190ea7539841d2204
Lucas Cherkewski <lucas@ecustom.ca>
2017-05-16 19:42:48 -0400

    add missing key

--- src/components/Talent/Groups/CardList.js
+++ src/components/Talent/Groups/CardList.js
@@ -10,7 +10,7 @@ import GroupCard from './Card';
 const GroupCardList = ({ className, groups, onSelectGroup }) => (
     <div className={classNames("flex flex-wrap justify-around nr3", className)}>
         {groups.map((group) => (
-            <div className="mt3 pr3 w-50">
+            <div className="mt3 pr3 w-50" key={group.id}>
                 <div className="pointer bt bw2 b--transparent hover-b--blue-yonder" style={{ marginTop: '-4px' }} onClick={() => onSelectGroup(group.id)}>
                     <GroupCard
                         className="bg-animate hover-bg-white-70"
