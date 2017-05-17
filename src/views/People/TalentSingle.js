817bdaf1d6d14c4ef702552c1189226dd9f94f74
Lucas Cherkewski <lucas@ecustom.ca>
2017-05-17 11:41:58 -0400

    start to talent single view details

--- src/views/People/TalentSingle.js
+++ src/views/People/TalentSingle.js
@@ -21,6 +21,8 @@ class TalentSingleView extends React.Component {
             <div className="pv4 bg-near-white">
                 <div className="flex mh4">
                     <Box className="w-third mr3">
+                        <h2 className="f6 mt0 lh-title ttu">Talent Details</h2>
+
                         <h1 className="mb4 f3">{this.props.talent.firstName} {this.props.talent.lastName}</h1>
                         <img className="db w-100 ba bw1 b--silver border-box" src={this.props.talent.imageSrc} alt={`Profile photo of ${this.props.talent.firstName}`}/>
 
@@ -35,9 +37,23 @@ class TalentSingleView extends React.Component {
                                 ))}
                             </ul>
                         </div>
+
+                        <p><i>Email</i></p>
+                        <p><i>Phone</i></p>
+                        <p><i>Emergency contact info</i></p>
                     </Box>
                     <Box className="w-two-thirds">
-                        <h2 className="f6 mt0 lh-title ttu">Talent Details</h2>
+                        <h2 className="f6 mt0 lh-title ttu">Talent Settings</h2>
+
+                        <p><i>Pay rate (hourly and overtime)</i></p>
+
+                        <p className="mt3">
+                            <label className="f6 db" htmlFor="talent_notes">Notes</label>
+                            <textarea className="mt2 w-100" name="talent_notes" id="talent_notes" aria-describedby="talent_notes_desc" cols="30" rows="5"/>
+                            <small className="f6 black-60" id="talent_notes_desc">Only managers can read these notes.</small>
+                        </p>
+
+                        <p><i>Metadata</i></p>
                     </Box>
                 </div>
             </div>
