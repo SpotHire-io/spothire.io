9e08251c8b250260369b6e1017c2aa835ce587eb
Lucas Cherkewski <lucas@ecustom.ca>
2017-05-17 11:34:32 -0400

    tweak margins to square photos up

--- src/views/People/TalentSingle.js
+++ src/views/People/TalentSingle.js
@@ -27,7 +27,7 @@ class TalentSingleView extends React.Component {
                         <div className="mt3">
                             <h3 className="f6 normal ma0">Other photos</h3>
 
-                            <ul className="mt2 flex flex-wrap pa0 list nr2">
+                            <ul className="mt2 nb2 flex flex-wrap pa0 list nr2">
                                 {[...Array(6).keys()].map((number) => (
                                     <li className="dib mt0 mb2 pa0 w-25 pr2">
                                         <img className="db w-100 ba bw1 b--silver border-box" src={this.props.talent.imageSrc} alt={`Profile photo of ${this.props.talent.firstName}`}/>
