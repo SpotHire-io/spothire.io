08e55c8f5d9ec955099449f5eaa7f6a94924be73
Lucas Cherkewski <lucas@ecustom.ca>
2017-05-18 17:54:40 -0400

    listing talent on opp page

--- src/views/Schedule/OpportunitySingle.js
+++ src/views/Schedule/OpportunitySingle.js
@@ -7,6 +7,10 @@ import classNames from 'classnames';
 
 import Box from '../../components/Global/Box';
 
+import BasicButton from '../../components/Buttons/BasicButton';
+
+import UserTable from '../../components/Talent/UserTable';
+
 import OpportunityModalBasicInfo from '../../components/Scheduling/OpportunityModal/BasicInfo';
 import ShiftManager from '../../components/Scheduling/ShiftManager';
 
@@ -35,9 +39,20 @@ class OpportunitySingleView extends React.Component {
 
                                 <OpportunityModalBasicInfo opportunity={this.props.opportunity} updateOpportunity={() => console.log('updating opportunity!')}/>
                             </Box>
+                            <Box className="mt4">
+                                <h2 className="f6 mt0 lh-title ttu">Talent</h2>
+
+                                <p>The following talent are invited or confirmed for the opportunity.</p>
+
+                                <UserTable className="mt3" enabledColumns={['avatar', 'name']}/>
+
+                                <div className="tr mt3">
+                                    <BasicButton className="button--positive">Invite Talent to Opportunity</BasicButton>
+                                </div>
+                            </Box>
                         </div>
                     </div>
-                    
+
                     <ShiftManager className="mt4 mh4"/>
                 </div>
             </div>
