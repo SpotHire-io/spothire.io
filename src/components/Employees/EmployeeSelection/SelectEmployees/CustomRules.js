6ffce913e57ce116e1d0223fac4520647f9c2dc7
Lucas Cherkewski <lucas@ecustom.ca>
2017-05-22 13:31:39 -0400

    key selection

--- src/components/Employees/EmployeeSelection/SelectEmployees/CustomRules.js
+++ src/components/Employees/EmployeeSelection/SelectEmployees/CustomRules.js
@@ -4,17 +4,42 @@ import PropTypes from 'prop-types';
 
 import classNames from 'classnames';
 
+import Select from 'react-select';
+
+import 'react-select/dist/react-select.css';
+
 class SelectCustomRules extends React.Component {
     constructor() {
         super();
 
-        this.state = {};
+        this.state = {
+            selectedKey: null,
+        };
     }
 
     render() {
         return (
             <div className={classNames('', this.props.className)}>
                 <p>Create custom rules to add all employees that match.</p>
+
+                <div className="mt3">
+                    <label className="db mb2 f6" htmlFor="custom_key">Key</label>
+                    <Select
+                        id="custom_key"
+                        className="mt0"
+                        options={[
+                            {
+                                label: 'Test',
+                                value: 'test'
+                            }
+                        ]}
+                        onChange={(newKey) => this.setState({ selectedKey: newKey.value })}
+                    />
+                    <small className="dib f6 black-60 lh-title mt2">
+                        The key you want to filter on. This can be a profile field, filled out by the employee, or a custom
+                        metadata key set by a manager.
+                    </small>
+                </div>
             </div>
         );
     }
