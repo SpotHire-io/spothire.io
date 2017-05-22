c28ba3647b4c27c95bf98030d7a536c18cb464ac
Lucas Cherkewski <lucas@ecustom.ca>
2017-05-22 12:13:27 -0400

    create group/custom rules subcomponents

---
+++ src/components/Employees/EmployeeSelection/SelectEmployees/Custom.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
+import classNames from 'classnames';
+
+class SelectCustomRules extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div className={classNames('', this.props.className)}>
+                <p>Create custom rules to add all employees that match.</p>
+            </div>
+        );
+    }
+}
+
+SelectCustomRules.defaultProps = {
+    className: '',
+};
+
+SelectCustomRules.propTypes = {
+    className: PropTypes.string,
+};
+
+export default SelectCustomRules
