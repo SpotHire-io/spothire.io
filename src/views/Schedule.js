7689baddb3719bc62155750da8d5bdb0059d4e97
Lucas Cherkewski <lucas@ecustom.ca>
2017-05-23 19:44:39 -0400

    switch to Select in Schedule view (will probably drop Filter component)

--- src/views/Schedule.js
+++ src/views/Schedule.js
@@ -10,6 +10,8 @@ import OverviewCalendar from '../components/Scheduling/OverviewCalendar';
 import FilterContainer from '../components/Filters/FilterContainer';
 import Filter from '../components/Filters/Filter';
 
+import Select from 'react-select';
+
 // storybook stuff
 import { linkTo } from '@kadira/storybook';
 
@@ -21,7 +23,7 @@ class ScheduleView extends React.Component {
         super();
 
         this.state = {
-            filteredUserIds: ''
+            filteredUserIds: []
         };
     }
 
@@ -31,39 +33,26 @@ class ScheduleView extends React.Component {
                 <div className="pa4 bg-near-white">
                     <div className="flex">
                         <FilterContainer className="mr3 w-third self-start">
-                            <Filter
-                                id="schedule_userId_filter"
-                                label="Managers"
-                                type="select"
-                                data={{
-                                    inputProps: {
-                                        id: 'schedule_userId_filter'
-                                    },
-                                    options: users
-                                        .filter((user) => user.type === 'admin' || user.type === 'manager')
-                                        .map((user) => {
-                                            return {
-                                                label: `${user.firstName} ${user.lastName}`,
-                                                value: user.id,
-                                            };
-                                        }),
-                                    selectConfig: {
-                                        multi: true,
-                                        joinValues: true,
-                                        simpleValue: true,
-                                        value: this.state.filteredUserIds,
-                                        onChange: (selectedIds) => this.setState({ filteredUserIds: selectedIds }),
-                                    }
-                                }}
+                            <Select
+                                multi
+                                options={users
+                                    .filter((user) => user.type === 'admin' || user.type === 'manager')
+                                    .map((user) => {
+                                        return {
+                                            label: `${user.firstName} ${user.lastName}`,
+                                            value: user.id,
+                                        };
+                                    })}
+                                value={this.state.filteredUserIds}
+                                onChange={(selectedUsers) => this.setState({ filteredUserIds: selectedUsers })}
                             />
                         </FilterContainer>
                         <Box className="flex-auto" title="Calendar">
-                            {this.state.filteredUserIds}
                             <OverviewCalendar
                                 events={this.props.events}
                                 filter={{
                                     property: 'userId',
-                                    values: this.state.filteredUserIds
+                                    values: this.state.filteredUserIds.map((user) => user.value)
                                 }}
                                 onSelectEvent={linkTo('Views', 'Schedule:OpportunitySingle')}
                             />
