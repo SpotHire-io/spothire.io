acb16909c9d9022f1a4263bab44d0fd78184b577
Lucas Cherkewski <lucas@ecustom.ca>
2017-05-23 17:27:55 -0400

    start to a filterable schedule view

--- src/views/Schedule.js
+++ src/views/Schedule.js
@@ -3,10 +3,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import OpportunitySchema from '../schemas/Opportunity';
 
-import MainMenu from '../components/Global/MainMenu';
 import Box from '../components/Global/Box';
 
 import OverviewCalendar from '../components/Scheduling/OverviewCalendar';
+
+import FilterContainer from '../components/Filters/FilterContainer';
+import Filter from '../components/Filters/Filter';
 
 // storybook stuff
 import { linkTo } from '@kadira/storybook';
@@ -14,22 +16,53 @@ import { linkTo } from '@kadira/storybook';
 class ScheduleView extends React.Component {
     constructor() {
         super();
+
+        this.state = {
+            filteredUserIds: []
+        };
     }
 
     render() {
         return (
             <div>
                 <div className="pa4 bg-near-white">
-                    <Box title="Calendar">
-                        <OverviewCalendar
-                            events={this.props.events}
-                            filter={{
-                                property: 'userId',
-                                values: [0]
-                            }}
-                            onSelectEvent={linkTo('Views', 'Schedule:OpportunitySingle')}
-                        />
-                    </Box>
+                    <div className="flex">
+                        <FilterContainer className="mr3 w-third self-start">
+                            <Filter
+                                id="schedule_userId_filter"
+                                label="Managers"
+                                type="select"
+                                data={{
+                                    inputProps: {
+                                        id: 'schedule_userId_filter'
+                                    },
+                                    options: [
+                                        {
+                                            value: '1',
+                                            label: 'Option 1'
+                                        },
+                                        {
+                                            value: '2',
+                                            label: 'Option 2'
+                                        }
+                                    ],
+                                    selectConfig: {
+                                        multi: true
+                                    }
+                                }}
+                            />
+                        </FilterContainer>
+                        <Box className="flex-auto" title="Calendar">
+                            <OverviewCalendar
+                                events={this.props.events}
+                                filter={{
+                                    property: 'userId',
+                                    values: this.state.filteredUserIds
+                                }}
+                                onSelectEvent={linkTo('Views', 'Schedule:OpportunitySingle')}
+                            />
+                        </Box>
+                    </div>
                 </div>
             </div>
         );
