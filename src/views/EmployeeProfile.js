294ce64c54818524685f198d0e79cb0ee8e04782
Lucas Cherkewski <lucas@ecustom.ca>
2017-05-24 15:42:31 -0400

    basic fields for employee profile

--- src/views/EmployeeProfile.js
+++ src/views/EmployeeProfile.js
@@ -22,7 +22,49 @@ class EmployeeProfileView extends React.Component {
 
         return (
             <div className="pa4 bg-near-white">
-                {this.props.employee.firstName}
+                <div className="flex">
+                    <Box className="w-third mr3">
+                        <h2 className="f6 mt0 lh-title ttu">Your Profile</h2>
+
+                        <h1 className="mb4 f3">{this.props.employee.firstName} {this.props.employee.lastName}</h1>
+                        <img className="db w-100 ba bw1 b--silver border-box pointer" onClick={() => alert('upload main profile photo interface')} src={this.props.employee.imageSrc} alt={`Profile photo of ${this.props.employee.firstName}`}/>
+
+                        <div className="mt3">
+                            <h3 className="f6 normal ma0">Other photos <small>(<span onClick={() => alert('upload secondary profile photo interface')} className="pointer underline hover-no-underline">add more</span>)</small></h3>
+
+                            <ul className="mt2 nb2 flex flex-wrap pa0 list nr2">
+                                {[...Array(6).keys()].map((number) => (
+                                    <li className="dib mt0 mb2 pa0 w-25 pr2">
+                                        <img className="db w-100 ba bw1 b--silver border-box" src={this.props.employee.imageSrc} alt={`Profile photo of ${this.props.employee.firstName}`}/>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    </Box>
+                    <div className="w-two-thirds">
+                        <Box>
+                            <h2 className="f6 mt0 lh-title ttu">Your Settings</h2>
+
+                            <div className="flex mt3">
+                                <div className="w-50 mr4">
+                                    <label className="f6 db mb2" htmlFor="employee_email">Email address</label>
+                                    <input className="w-100" type="email" id="employee_email" name="employee_email" defaultValue={this.props.employee.email}/>
+                                </div>
+
+                                <div className="w-50 mt0">
+                                    <label className="f6 db mb2" htmlFor="employee_phone">Phone number</label>
+                                    <input className="w-100" type="tel" id="employee_phone" name="employee_phone" defaultValue={this.props.employee.phone}/>
+                                </div>
+                            </div>
+
+                            <div className="mt3">
+                                <label className="f6 db mb2" htmlFor="employee_emergencyContactInformation">Emergency contact information</label>
+                                <input className="w-100" type="text" id="employee_emergencyContactInformation" name="employee_emergencyContactInformation" defaultValue={this.props.employee.emergencyContactInformation}/>
+                                <small className="f6 black-60 db mt2" id="employee_notes_desc">Who should a manager contact if something goes wrong? How can a manager contact them?</small>
+                            </div>
+                        </Box>
+                    </div>
+                </div>
             </div>
         );
     }
