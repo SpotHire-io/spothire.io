2d11b6cc601244529639440280d3e7203298bd29
Lucas Cherkewski <lucas@ecustom.ca>
2017-05-24 21:29:47 -0400

    add message if no selections

--- src/components/Employees/EmployeeSelection/ReviewSelectedEmployees.js
+++ src/components/Employees/EmployeeSelection/ReviewSelectedEmployees.js
@@ -10,6 +10,32 @@ import Icon from 'react-geomicons';
 class ReviewSelectedEmployees extends React.Component {
     constructor() {
         super();
+
+        this.renderSelections = this.renderSelections.bind(this);
+        this.renderEmptySelectionsMessage = this.renderEmptySelectionsMessage.bind(this);
+    }
+
+    renderSelections(category) {
+        return (
+            <ul className="list pa0 ma0">
+                {category.selections.map((selection) => {
+                    return (
+                        <li key={selection.id} className="mt0 flex">
+                            {category.renderMethod(selection, 'flex-auto')}
+                            <div className="tr">
+                                <Icon color="#555555" name="close" className="pointer" onClick={() => this.props.unSelectById(category.key, selection.id)}/>
+                            </div>
+                        </li>
+                    );
+                })}
+            </ul>
+        )
+    }
+
+    renderEmptySelectionsMessage(category) {
+        return (
+            <p className="mt0 i">No {category.title.toLowerCase()} provided.</p>
+        )
     }
 
     render() {
@@ -20,18 +46,7 @@ class ReviewSelectedEmployees extends React.Component {
                         <section className="mb3" key={category.key}>
                             <h4 className="f6 normal mt0 mb2">{category.title}</h4>
 
-                            <ul className="list pa0 ma0">
-                                {category.selections.map((selection) => {
-                                    return (
-                                        <li key={selection.id} className="mt0 flex">
-                                            {category.renderMethod(selection, 'flex-auto')}
-                                            <div className="tr">
-                                                <Icon color="#555555" name="close" className="pointer" onClick={() => this.props.unSelectById(category.key, selection.id)}/>
-                                            </div>
-                                        </li>
-                                    );
-                                })}
-                            </ul>
+                            {(category.selections.length > 0) ? this.renderSelections(category) : this.renderEmptySelectionsMessage(category)}
                         </section>
                     );
                 })}
