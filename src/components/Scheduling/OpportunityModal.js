40fa30a376a0119f941b76aa12d5fbb6f5e56c0f
Lucas Cherkewski <lucas@ecustom.ca>
2017-05-24 21:37:16 -0400

    remove Employees tab from OppM

--- src/components/Scheduling/OpportunityModal.js
+++ src/components/Scheduling/OpportunityModal.js
@@ -5,12 +5,9 @@ import OpportunitySchema from '../../schemas/Opportunity';
 
 import Modal from 'react-modal';
 
-import SectionSwitcher from '../Miscellaneous/SectionSwitcher';
-
 import BasicButton from '../Buttons/BasicButton';
 
 import OpportunityModalBasicInfo from './OpportunityModal/BasicInfo';
-import OpportunityModalEmployees from './OpportunityModal/Employees';
 
 class OpportunityModal extends React.Component {
     constructor() {
@@ -28,26 +25,14 @@ class OpportunityModal extends React.Component {
                 closeTimeoutMS={100}
             >
                 <h2 className="absolute top-0 right-0 left-0 bg-blue-yonder white bb bw1 b--white-40 ma0 pa3 f5">New Opportunity</h2>
-                <SectionSwitcher
-                    className="mt4 pt1"
-                    secondaryMenuClassName="mb3"
-                    sections={[
-                        {
-                            key: 'basic',
-                            name: 'Basic Info',
-                            content: <OpportunityModalBasicInfo {...this.props}/>
-                        },
-                        {
-                            key: 'employees',
-                            name: 'Employees',
-                            content: <OpportunityModalEmployees {...this.props}/>
-                        }
-                    ]}
-                />
+
+                <div className="mt4 pt3">
+                    <OpportunityModalBasicInfo {...this.props}/>
+                </div>
 
                 <div className="tr">
                     <BasicButton className="button--neutral mt3" onClick={() => this.props.closeModal()}>Cancel</BasicButton>
-                    <BasicButton className="button--positive mt3 ml3" onClick={() => this.props.closeModal()}>Create</BasicButton>
+                    <BasicButton className="button--positive mt3 ml3" onClick={this.props.addOpportunity}>Create</BasicButton>
                 </div>
             </Modal>
         )
@@ -58,7 +43,8 @@ OpportunityModal.propTypes = {
     isOpen: PropTypes.bool.isRequired,
     closeModal: PropTypes.func.isRequired,
     updateOpportunity: PropTypes.func.isRequired,
-    opportunity: OpportunitySchema
+    opportunity: OpportunitySchema,
+    addOpportunity: PropTypes.func.isRequired,
 };
 
 export default OpportunityModal;
