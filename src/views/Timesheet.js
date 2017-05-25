40f3307c631e7e1f4b114f21fd5df0e60ff441ab
Lucas Cherkewski <lucas@ecustom.ca>
2017-05-25 13:23:59 -0400

    remove unused FilterContainer on Timesheet

--- src/views/Timesheet.js
+++ src/views/Timesheet.js
@@ -42,90 +42,27 @@ class TimesheetView extends React.Component {
         }
 
         return (
-            <div className="flex bg-near-white pa4">
-                <FilterContainer className="mr3 w-third self-start">
-                    <Filter
-                        id="text1"
-                        label="Other text filter"
-                        type="text"
-                        data={{
-                            placeholder: 'A text filter'
-                        }}
-                    />
-                    <Filter
-                        id="text2"
-                        label="Text filter"
-                        type="text"
-                        data={{
-                            placeholder: 'Another text filter'
-                        }}
-                    />
-                    <Filter
-                        id="select1"
-                        label="Single select"
-                        type="select"
-                        data={{
-                            inputProps: {
-                                id: 'select1'
-                            },
-                            options: [
-                                {
-                                    value: '1',
-                                    label: 'Option 1'
-                                },
-                                {
-                                    value: '2',
-                                    label: 'Option 2'
-                                }
-                            ]
-                        }}
-                    />
-                    <Filter
-                        id="select2"
-                        label="Multi select"
-                        type="select"
-                        data={{
-                            inputProps: {
-                                id: 'select2'
-                            },
-                            options: [
-                                {
-                                    value: '1',
-                                    label: 'Option 1'
-                                },
-                                {
-                                    value: '2',
-                                    label: 'Option 2'
-                                }
-                            ],
-                            selectConfig: {
-                                multi: true
-                            }
-                        }}
-                    />
-                </FilterContainer>
-                <div className="w-two-thirds">
-                    <Box>
-                        <h2 className="f6 mt0 lh-title ttu">Summary</h2>
-                        <TimetableSummary users={summarizedUsers}/>
-                    </Box>
+            <div className="bg-near-white pa4">
+                <Box>
+                    <h2 className="f6 mt0 lh-title ttu">Summary</h2>
+                    <TimetableSummary users={summarizedUsers}/>
+                </Box>
 
-                    <BoxConnector isActive={this.state.searchQuery.length > 0}/>
+                <BoxConnector isActive={this.state.searchQuery.length > 0}/>
 
-                    <Box>
-                        <p>
-                            <label className="f6 db" htmlFor="employees_search">Search</label>
-                            <input className="mt2 w-100" type="text" id="employees_search" name="employees_search" value={this.state.searchQuery} onChange={(e) => this.setState({ searchQuery: e.target.value })}/>
-                        </p>
-                    </Box>
+                <Box>
+                    <p>
+                        <label className="f6 db" htmlFor="employees_search">Search</label>
+                        <input className="mt2 w-100" type="text" id="employees_search" name="employees_search" value={this.state.searchQuery} onChange={(e) => this.setState({ searchQuery: e.target.value })}/>
+                    </p>
+                </Box>
 
-                    <BoxConnector isActive={this.state.searchQuery.length > 0}/>
+                <BoxConnector isActive={this.state.searchQuery.length > 0}/>
 
-                    <SelectableTimesheetTable
-                        users={filteredUsers}
-                        onUpdateSelectedUsers={(selectedUserIds) => this.setState({ selectedUserIds })}
-                    />
-                </div>
+                <SelectableTimesheetTable
+                    users={filteredUsers}
+                    onUpdateSelectedUsers={(selectedUserIds) => this.setState({ selectedUserIds })}
+                />
             </div>
         );
     }
