269e2d4ca2cb1936eb94db94afc967285b2ad2ae
Lucas Cherkewski <lucas@ecustom.ca>
2017-06-01 10:16:13 -0400

    add RSVP details for opp

--- src/views/Employee/Schedule/OpportunitySingle.js
+++ src/views/Employee/Schedule/OpportunitySingle.js
@@ -21,50 +21,59 @@ class EmployeeOpportunitySingleView extends React.Component {
     render() {
         return (
             <div className={classNames('pa4 bg-near-white flex', this.props.className)}>
-                <Box className="w-third mr3">
-                    <h2 className="f6 mt0 lh-title ttu">Opportunity Details</h2>
+                <div className="w-third mr3">
+                    <Box>
+                        <h2 className="f6 mt0 lh-title ttu">Opportunity Details</h2>
 
-                    <h1 className="mb0 f3">{this.props.opportunity.title}</h1>
+                        <h1 className="mb0 f3">{this.props.opportunity.title}</h1>
 
-                    <dl className="ma0 flex flex-wrap nr3">
-                        {[
-                            {
-                                id: 'startDate',
-                                label: 'From',
-                                content: moment(this.props.opportunity.selectedDates.start).format((this.props.opportunity.isAllDay) ? 'MMMM Do, YYYY [(all day)]' : 'MMMM, Do, YYYY h:mm a'),
-                            },
-                            {
-                                id: 'endDate',
-                                label: 'To',
-                                content: moment(this.props.opportunity.selectedDates.end).format((this.props.opportunity.isAllDay) ? 'MMMM Do, YYYY [(all day)]' : 'MMMM, Do, YYYY h:mm a'),
-                            },
-                            {
-                                id: 'location',
-                                label: 'Location',
-                                content: '100 Street St., Toronto',
-                                isFullWidth: true,
-                            },
-                            {
-                                id: 'notes',
-                                label: 'Notes',
-                                content: 'Elementum eu interdum eu tincidunt in leo maecenas pulvinar lobortis.',
-                                isFullWidth: true,
-                            },
-                        ].map((section) => (
-                            <div
-                                className={classNames({
-                                    'pr3 mt3': true,
-                                    'w-50': section.isFullWidth !== true,
-                                    'w-100': section.isFullWidth
-                                })}
-                                key={section.id}
-                            >
-                                <dt className="f6 mb2">{section.label}</dt>
-                                <dd className="ma0">{section.content}</dd>
-                            </div>
-                        ))}
-                    </dl>
-                </Box>
+                        <dl className="ma0 flex flex-wrap nr3">
+                            {[
+                                {
+                                    id: 'startDate',
+                                    label: 'From',
+                                    content: moment(this.props.opportunity.selectedDates.start).format((this.props.opportunity.isAllDay) ? 'MMMM Do, YYYY [(all day)]' : 'MMMM, Do, YYYY h:mm a'),
+                                },
+                                {
+                                    id: 'endDate',
+                                    label: 'To',
+                                    content: moment(this.props.opportunity.selectedDates.end).format((this.props.opportunity.isAllDay) ? 'MMMM Do, YYYY [(all day)]' : 'MMMM, Do, YYYY h:mm a'),
+                                },
+                                {
+                                    id: 'location',
+                                    label: 'Location',
+                                    content: '100 Street St., Toronto',
+                                    isFullWidth: true,
+                                },
+                                {
+                                    id: 'notes',
+                                    label: 'Notes',
+                                    content: 'Elementum eu interdum eu tincidunt in leo maecenas pulvinar lobortis.',
+                                    isFullWidth: true,
+                                },
+                            ].map((section) => (
+                                <div
+                                    className={classNames({
+                                        'pr3 mt3': true,
+                                        'w-50': section.isFullWidth !== true,
+                                        'w-100': section.isFullWidth
+                                    })}
+                                    key={section.id}
+                                >
+                                    <dt className="f6 mb2">{section.label}</dt>
+                                    <dd className="ma0">{section.content}</dd>
+                                </div>
+                            ))}
+                        </dl>
+                    </Box>
+                    <Box className="mt3">
+                        <h2 className="f6 mt0 lh-title ttu">RSVP</h2>
+
+                        <p className="mt0">You are <strong>confirmed</strong> for this opportunity.</p>
+
+                        <BasicButton className="button--negative mt3 w-100">Cancel Confirmation</BasicButton>
+                    </Box>
+                </div>
                 <div className="w-two-thirds">
                     <Box>
                         <h2 className="f6 mt0 lh-title ttu">Posts</h2>
