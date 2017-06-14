e6e22fc24fd35642f2d6a8d6a798c30ef3d275e3
Lucas Cherkewski <lucas@ecustom.ca>
2017-06-14 14:58:57 -0400

    account for two types of groups within group schema itself

--- src/schemas/Group.js
+++ src/schemas/Group.js
@@ -1,12 +1,17 @@
 import PropTypes from 'prop-types';
 
 import PersonSchema from './Person';
+import CustomFilterSchema from './CustomFilter';
 
 const GroupSchema = PropTypes.shape({
     id: PropTypes.number.isRequired,
     name: PropTypes.string.isRequired,
     description: PropTypes.string,
-    employees: PropTypes.arrayOf(PersonSchema),
+    type: PropTypes.oneOf('static', 'virtual').isRequired,
+    employees: PropTypes.oneOfType([
+        PropTypes.arrayOf(PersonSchema),
+        PropTypes.arrayOf(CustomFilterSchema),
+    ]),
 });
 
 export default GroupSchema;
