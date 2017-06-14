c8c09890839b91af67def18fa0ad1932a15f914c
Lucas Cherkewski <lucas@ecustom.ca>
2017-06-14 11:14:47 -0400

    render AvailabilityViewer

--- src/components/Profile/AvailabilityViewer.js
+++ src/components/Profile/AvailabilityViewer.js
@@ -40,7 +40,33 @@ const mockAvailability = {
 
 const AvailabilityViewer = ({ className, employee }) => {
     return (
-        <div className={classNames(className)}/>
+        <div className={classNames(className)}>
+            <ol className="list ma0 pa0 nt2">
+                {
+                    Object.keys(mockAvailability).map((day, index) => {
+                        const currentDay = mockAvailability[day];
+
+                        return (
+                            <li key={day} className="ma0 ph0 pv2">
+                                <p className="b mt0 mb2">{day.charAt(0).toUpperCase() + day.slice(1)}</p>
+                                {
+                                    (currentDay !== false)
+                                        ? (
+                                            <ol className="list pa0">
+                                                {currentDay.map((timeSlot, timeSlotIndex) => (
+                                                    <li key={timeSlot.id} className="pa0">
+                                                        {moment(timeSlot.start).format('h:mm a')} to {moment(timeSlot.end).format('h:mm a')}
+                                                    </li>
+                                                ))}
+                                            </ol>
+                                        ) : <p className="i mt0">No availability set.</p>
+                                }
+                            </li>
+                        );
+                    })
+                }
+            </ol>
+        </div>
     );
 };
 
