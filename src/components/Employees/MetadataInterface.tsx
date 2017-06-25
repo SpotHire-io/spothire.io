b9372db4eb0edc018b49f1c1260652e6ee9a4f5b
Denis <carriere.denis@gmail.com>
2017-06-25 18:10:31 -0400

    Update MetadataInterface

--- src/components/Employees/MetadataInterface.tsx
+++ src/components/Employees/MetadataInterface.tsx
@@ -1,8 +1,8 @@
+import * as React from 'react'
+import * as classNames from 'classnames'
+import * as Select from 'react-select'
 import Icon from '../../../libraries/react-geomicons'
-import * as React from 'react';
-import * as classNames from 'classnames';
-import BasicButton from '../../components/Buttons/BasicButton';
-import * as Select from 'react-select';
+import BasicButton from '../../components/Buttons/BasicButton'
 import {MetaPairs, Metadata, Person, MetadataType} from '../../schemas'
 
 
@@ -32,105 +32,91 @@ const metaTypes = [
         label: 'Yes/No',
         input: 'checkbox',
     },
-];
+]
 
 export default class MetadataInterface extends React.Component<Props, State> {
-    public static defaultProps = {
-        className: ''
-    }
-
     constructor(props: Props) {
-        super();
-
-        this.toggleEditingMetaPair = this.toggleEditingMetaPair.bind(this);
-
-        this.deleteMetaPair = this.deleteMetaPair.bind(this);
-        this.createMetaPair = this.createMetaPair.bind(this);
-        this.updateMetaPairFieldByIndex = this.updateMetaPairFieldByIndex.bind(this);
-
-        this.renderMetaPairValueEditor = this.renderMetaPairValueEditor.bind(this);
-
+        super()
         this.state = {
             currentlyEditingMetaPairId: null,
             metaPairs: props.employee.metadata
-        };
+        }
     }
 
-    toggleEditingMetaPair(metaPairId: number) {
-        let currentlyEditingMetaPairId = this.state.currentlyEditingMetaPairId;
+    private toggleEditingMetaPair = (metaPairId: number) => {
+        let currentlyEditingMetaPairId = this.state.currentlyEditingMetaPairId
 
         // Unset the currently editing metaPair if we’re toggling that metaPair. Else, set to the new metaPair ID.
         if (metaPairId === currentlyEditingMetaPairId) {
-            currentlyEditingMetaPairId = null;
+            currentlyEditingMetaPairId = null
         } else {
-            currentlyEditingMetaPairId = metaPairId;
+            currentlyEditingMetaPairId = metaPairId
         }
 
-        return this.setState({ currentlyEditingMetaPairId });
+        return this.setState({ currentlyEditingMetaPairId })
     }
 
-    deleteMetaPair(metaPairKey: string) {
-        let metaPairs = [...this.state.metaPairs];
+    private deleteMetaPair = (metaPairKey: string) => {
+        let metaPairs = [...this.state.metaPairs]
 
         // Drop the metaPair by finding its index
-        metaPairs.splice(metaPairs.findIndex((metaPair) => metaPair.key === metaPairKey), 1);
+        metaPairs.splice(metaPairs.findIndex((metaPair) => metaPair.key === metaPairKey), 1)
 
-        return this.setState({ metaPairs });
+        return this.setState({ metaPairs })
     }
 
-    createMetaPair() {
-        let metaPairs = [...this.state.metaPairs];
+    private createMetaPair = () => {
+        let metaPairs = [...this.state.metaPairs]
 
         // extract the highest ID currently existing so we have something to mock
         const highestId = metaPairs.reduce((currentHighestId, metaPair) => {
-            return Math.max(currentHighestId, metaPair.id);
-        }, -1);
+            return Math.max(currentHighestId, metaPair.id)
+        }, -1)
 
         metaPairs.push({
             id: highestId + 1,
             key: '',
             type: 'string',
             value: ''
-        });
+        })
 
-        return this.setState({ metaPairs, currentlyEditingMetaPairId: highestId + 1 });
+        return this.setState({ metaPairs, currentlyEditingMetaPairId: highestId + 1 })
     }
 
-    updateMetaPairFieldByIndex(metaPairIndex: number, field: string, value: string) {
-        let metaPairs: any = [...this.state.metaPairs];
-        metaPairs[metaPairIndex][field] = value;
+    private updateMetaPairFieldByIndex = (metaPairIndex: number, field: string, value: string) => {
+        let metaPairs: any = [...this.state.metaPairs]
+        metaPairs[metaPairIndex][field] = value
 
-        return this.setState({ metaPairs });
+        return this.setState({ metaPairs })
     }
 
-    renderControls(metaPair: Metadata) {
+    private renderControls = (metaPair: Metadata) => {
         return (
             <div className="tr self-center ml2">
                 <Icon color="#555555" name={(metaPair.id !== this.state.currentlyEditingMetaPairId) ? 'compose' : 'check'} className="pointer mt1" onClick={() => this.toggleEditingMetaPair(metaPair.id)}/>
                 <Icon color="#555555" name="close" className="pointer mt1 ml2" onClick={() => this.deleteMetaPair(metaPair.key)}/>
             </div>
-        );
+        )
     }
 
-    renderMetaPairValueEditor(metadata: Metadata, metadataIndex: number) {
-        let metaPairValueEditor: JSX.Element;
-        let metaTypeConfig = metaTypes.find((metaType) => metaType.type === metadata.type);
+    private renderMetaPairValueEditor = (metadata: Metadata, metadataIndex: number) => {
+        let metaPairValueEditor: JSX.Element
+        let metaTypeConfig = metaTypes.find((metaType) => metaType.type === metadata.type)
 
         switch (metadata.type) {
             case 'string':
             case 'number':
-                metaPairValueEditor = <input className="pa1 ma0 nl1 w-100" type={metaTypeConfig.type} value={metadata.value} onChange={(e) => this.updateMetaPairFieldByIndex(metadataIndex, 'value', e.target.value)} placeholder="value"/>;
-                break;
+                metaPairValueEditor = <input className="pa1 ma0 nl1 w-100" type={metaTypeConfig.type} value={metadata.value} onChange={(e) => this.updateMetaPairFieldByIndex(metadataIndex, 'value', e.target.value)} placeholder="value"/>
+                break
             case 'boolean':
-                metaPairValueEditor = <input className="pa1 ma0 nl1 w-100" type="checkbox" checked={!!metadata.value} onChange={(e) => this.updateMetaPairFieldByIndex(metadataIndex, 'value', String(e.target.checked))}/>;
-                break;
-            default: break;
+                metaPairValueEditor = <input className="pa1 ma0 nl1 w-100" type="checkbox" checked={!!metadata.value} onChange={(e) => this.updateMetaPairFieldByIndex(metadataIndex, 'value', String(e.target.checked))}/>
+                break
+            default: break
         }
 
-        return metaPairValueEditor;
+        return metaPairValueEditor
     }
-    render() {
-        console.log(this.state)
+    public render() {
         return (
             <div className={classNames(this.props.className)}>
                 <ul className="list ma0 pa0 bg-near-white bb b--black-20">
@@ -161,7 +147,7 @@ export default class MetadataInterface extends React.Component<Props, State> {
                                                     return {
                                                         value: type.type,
                                                         label: type.label,
-                                                    };
+                                                    }
                                                 })}
                                             />
                                         </dt>
@@ -179,6 +165,6 @@ export default class MetadataInterface extends React.Component<Props, State> {
                     <BasicButton className="button--positive" onClick={this.createMetaPair}>Add Metadata</BasicButton>
                 </div>
             </div>
-        );
+        )
     }
 }
