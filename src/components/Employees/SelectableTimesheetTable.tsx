fc0d3021c656262ce1b3284c8a8f2faa724bc648
Denis <carriere.denis@gmail.com>
2017-06-25 15:36:11 -0400

    Refactor TS SelectableTimesheetTable

--- src/components/Employees/SelectableTimesheetTable.tsx
+++ src/components/Employees/SelectableTimesheetTable.tsx
@@ -1,16 +1,36 @@
-import 'moment/locale/en-ca';
-import 'moment-duration-format';
-import React from 'react';
-import moment from 'moment';
-import classNames from 'classnames';
-import { Checkbox } from 'rebass';
-import { Table, Thead, Th, Tr, Td } from 'reactable';
-import { CSSTransitionGroup } from 'react-transition-group';
-// import * as Schemas from '../../schemas'
+import 'moment/locale/en-ca'
+import 'moment-duration-format'
+import * as React from 'react'
+import * as moment from 'moment'
+import * as classNames from 'classnames'
+const { Checkbox } = require('rebass')
+const { Table, Thead, Th, Tr, Td } = require('reactable')
+import { CSSTransitionGroup } from 'react-transition-group'
+import * as Schemas from '../../schemas'
 
-// import userData from '../../data/people.json';
+// const userData = require('../../data/people.json');
 
-class SelectableTimesheetTable extends React.Component {
+interface Props {
+    className?: string
+    users: Schemas.Person[]
+    hasShadow?: boolean
+    tableProps?: any
+    onUpdateSelectedUsers?: Function
+}
+
+interface State {
+    selectedUserIds: number[]
+}
+
+const commonCellClasses = 'pa3'
+
+export default class SelectableTimesheetTable extends React.Component<Props, State> {
+    public static defaultProps = {
+        className: '',
+        hasShadow: true,
+        tableProps: {},
+        onUpdateSelectedUsers: (selectedUserIds: number) => console.log(selectedUserIds)
+    }
     constructor() {
         super();
 
@@ -22,14 +42,12 @@ class SelectableTimesheetTable extends React.Component {
         this.selectAllUsers = this.selectAllUsers.bind(this);
         this.unselectAllUsers = this.unselectAllUsers.bind(this);
 
-        this.commonCellClasses = 'pa3';
-
         this.state = {
             selectedUserIds: []
         };
     }
 
-    toggleUser(userId) {
+    toggleUser(userId: number) {
         const selectedUserIds = [...this.state.selectedUserIds];
 
         if (this.state.selectedUserIds.indexOf(userId) === -1) {
@@ -54,7 +72,7 @@ class SelectableTimesheetTable extends React.Component {
         this.props.onUpdateSelectedUsers([]);
     }
 
-    renderUserRow(user) {
+    renderUserRow(user: Schemas.Person) {
         const userClasses = classNames({
             'ph3 pa2 mt0 hover-bg-black-10 pointer': true,
             'bg-washed-red': user.hours.submitted > user.hours.worked,
@@ -64,37 +82,38 @@ class SelectableTimesheetTable extends React.Component {
         return (
             <Tr key={user.id} className={userClasses} onClick={() => this.toggleUser(user.id)}>
                 {this.renderUserCell('avatar', () => {
-                    let innerContent;
+                    let innerContent: JSX.Element;
 
                     if (this.state.selectedUserIds.indexOf(user.id) === -1) {
-                        innerContent = (<img key={'image-' + user.id} className="db w1 h1 br-100 v-btm" src={user.imageSrc} alt="inner content"/>);
+                        innerContent = (<img key={'image-' + user.id} className='db w1 h1 br-100 v-btm' src={user.imageSrc} alt='inner content'/>);
                     } else {
-                        innerContent = (<Checkbox key={'checkbox-' + user.id} theme="success" style={{ display: 'inline' }} checked label="" name="" onClick={(e) => e.stopPropagation()}/>);
+                        innerContent = (<Checkbox key={'checkbox-' + user.id} theme='success' style={{ display: 'inline' }} checked label='' name='' onClick={(e: any) => e.stopPropagation()}/>);
                     }
 
                     return (
                         <CSSTransitionGroup
-                            transitionName="animation__image-checkbox"
+                            transitionName='animation__image-checkbox'
                             transitionEnterTimeout={150}
                             transitionLeaveTimeout={150}
-                            className="sh-rebass-checkbox-mr0 relative w1 h1"
-                            component="div"
+                            className='sh-rebass-checkbox-mr0 relative w1 h1'
+                            component='div'
                         >
                             {innerContent}
                         </CSSTransitionGroup>
                     );
                 }, 'pr0 no-select')}
                 {this.renderUserCell('name', user.firstName + ' ' + user.lastName)}
-                {this.renderUserCell('hours_worked', moment.duration(user.hours.worked, 'hours').format('h:mm'))}
-                {this.renderUserCell('hours_submitted', moment.duration(user.hours.submitted, 'hours').format('h:mm'))}
+                {/*BEFORE REFACTORING => {moment.duration(user.hours.worked, 'hours').format('h:mm')}*/}
+                {this.renderUserCell('hours_worked', moment.duration(user.hours.worked, 'hours').asHours())}
+                {this.renderUserCell('hours_submitted', moment.duration(user.hours.submitted, 'hours').asHours())}
             </Tr>
         );
     }
 
-    renderUserCell(column, value, className) {
+    renderUserCell(column: string, value: any, className?: string): any {
         const cellClasses = classNames({
             'bb b--black-20': true,
-            [this.commonCellClasses]: true,
+            [commonCellClasses]: true,
             [className]: true
         });
 
@@ -108,10 +127,10 @@ class SelectableTimesheetTable extends React.Component {
         );
     }
 
-    renderHeaderCell(column, value, className) {
+    renderHeaderCell(column: string, value: any, className: string) {
         const cellClasses = classNames({
             'tl bg-teal white bb bw1 b--white-40': true,
-            [this.commonCellClasses]: true,
+            [commonCellClasses]: true,
             [className]: true
         });
 
@@ -134,13 +153,13 @@ class SelectableTimesheetTable extends React.Component {
 
         return (
             <div className={wrapperClasses}>
-                <Table {...this.props.tableProps} style={{ width: 'calc(100% + 1px)' }} cellSpacing="0" sortable={['name']}>
+                <Table {...this.props.tableProps} style={{ width: 'calc(100% + 1px)' }} cellSpacing='0' sortable={['name']}>
                     <Thead>
                     {this.renderHeaderCell('avatar', () => {
                         if (this.state.selectedUserIds.length === this.props.users.length) {
-                            return (<div className="sh-rebass-checkbox-mr0"><Checkbox theme="white" style={{ display: 'inline' }} checked label="" name="" onClick={() => this.unselectAllUsers()}/></div>);
+                            return (<div className='sh-rebass-checkbox-mr0'><Checkbox theme='white' style={{ display: 'inline' }} checked label='' name='' onClick={() => this.unselectAllUsers()}/></div>);
                         } else {
-                            return (<div className="sh-rebass-checkbox-mr0"><Checkbox theme="white" style={{ display: 'inline' }} label="" name=""  onClick={() => this.selectAllUsers()}/></div>);
+                            return (<div className='sh-rebass-checkbox-mr0'><Checkbox theme='white' style={{ display: 'inline' }} label='' name=''  onClick={() => this.selectAllUsers()}/></div>);
                         }
                     }, 'w1')}
                     {this.renderHeaderCell('name', 'Name', '')}
@@ -153,20 +172,3 @@ class SelectableTimesheetTable extends React.Component {
         )
     }
 }
-
-SelectableTimesheetTable.defaultProps = {
-    className: '',
-    hasShadow: true,
-    tableProps: {},
-    onUpdateSelectedUsers: (selectedUserIds) => console.log(selectedUserIds)
-};
-
-// SelectableTimesheetTable.propTypes = {
-//     className: PropTypes.string,
-//     users: PropTypes.arrayOf(PersonSchema).isRequired,
-//     hasShadow: PropTypes.bool,
-//     tableProps: PropTypes.object,
-//     onUpdateSelectedUsers: PropTypes.func,
-// };
-
-export default SelectableTimesheetTable;
