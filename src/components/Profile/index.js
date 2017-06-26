feb2410c7bfa0bfc5f5942dc2b74c303b1f5ac4b
Denis <carriere.denis@gmail.com>
2017-06-26 01:47:35 -0400

    Drop index Profile

--- src/components/Profile/index.js
+++
@@ -1,9 +0,0 @@
-import NewTimeOffRequestModal from './NewTimeOffRequestModal'
-import AvailabilityEditor from './AvailabilityEditor'
-import TimeOffRequestList from './TimeOffRequestList'
-
-export {
-  NewTimeOffRequestModal,
-  AvailabilityEditor,
-  TimeOffRequestList
-}
