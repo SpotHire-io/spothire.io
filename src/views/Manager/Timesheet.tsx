38acbcc375670737604b439f1e4173c5bf13e6c2
Denis <carriere.denis@gmail.com>
2017-06-26 02:22:54 -0400

    Refactor Timesheet

--- src/views/Manager/Timesheet.tsx
+++ src/views/Manager/Timesheet.tsx
@@ -1,74 +1,91 @@
-import 'react-select/dist/react-select.css';
-import * as React from 'react';
-import * as Select from 'react-select';
-import * as PropTypes from 'prop-types';
-import Box from '../../components/Global/Box';
-import BoxConnector from '../../components/Global/BoxConnector';
-import PersonSchema from '../../schemas/Person';
-import TimetableSummary from '../../components/Employees/TimetableSummary';
-import SelectableTimesheetTable from '../../components/Employees/SelectableTimesheetTable';
+import 'react-select/dist/react-select.css'
+import * as React from 'react'
+import * as Select from 'react-select'
+import * as PropTypes from 'prop-types'
+import Box from '../../components/Global/Box'
+import BoxConnector from '../../components/Global/BoxConnector'
+import {Person} from '../../schemas'
+import TimetableSummary from '../../components/Employees/TimetableSummary'
+import SelectableTimesheetTable from '../../components/Employees/SelectableTimesheetTable'
 
-class TimesheetView extends React.Component {
+const discrepancyTypesOptions = [
+    {
+        value: 'underSubmitted',
+        label: 'Employees with more hours worked than submitted',
+    },
+    {
+        value: 'overSubmitted',
+        label: 'Employees with more hours submitted than worked',
+    },
+]
+
+interface Props {
+    className?: string
+    employees: Person[]
+}
+
+interface State {
+    nameSearch: string
+    discrepancyType?: DiscrepancyTypes
+    selectedUserIds: number[]
+}
+
+type DiscrepancyTypes = {
+    value: string
+    label: string
+}
+
+export default class TimesheetView extends React.Component<Props, State> {
     constructor() {
-        super();
-
-        this.discrepancyTypes = [
-            {
-                value: 'underSubmitted',
-                label: 'Employees with more hours worked than submitted',
-            },
-            {
-                value: 'overSubmitted',
-                label: 'Employees with more hours submitted than worked',
-            },
-        ];
+        super()
 
         this.state = {
             nameSearch: '',
-            selectedUserIds: [],
-            discrepancyType: null, // oneOf[null, 'overSubmitted', 'underSubmitted']
-        };
+            selectedUserIds: []
+        }
     }
 
     render() {
-        let filteredUsers = this.props.employees.filter((user) => `${user.firstName} ${user.lastName}`.indexOf(this.state.nameSearch) > -1);
-
-        if (this.state.discrepancyType === 'underSubmitted') {
-            filteredUsers = filteredUsers.filter((user) => user.hours.worked > user.hours.submitted)
-        } else if (this.state.discrepancyType === 'overSubmitted') {
-            filteredUsers = filteredUsers.filter((user) => user.hours.submitted > user.hours.worked)
+        let filteredUsers = this.props.employees.filter((user: Person) => `${user.firstName} ${user.lastName}`.indexOf(this.state.nameSearch) > -1)
+        console.log(filteredUsers)
+        console.log(this.state.discrepancyType)
+        switch (this.state.discrepancyType.value) {
+        case 'underSubmitted':
+            filteredUsers = filteredUsers.filter((user: Person) => user.hours.worked > user.hours.submitted)
+            break
+        case 'overSubmitted':
+            filteredUsers = filteredUsers.filter((user: Person) => user.hours.submitted > user.hours.worked)
         }
 
-        let summarizedUsers = filteredUsers;
+        let summarizedUsers = filteredUsers
 
         if (this.state.selectedUserIds.length > 0) {
-            summarizedUsers = filteredUsers.filter((user) => this.state.selectedUserIds.includes(user.id));
+            summarizedUsers = filteredUsers.filter((user: Person) => this.state.selectedUserIds.includes(user.id))
         }
 
         return (
-            <div className="bg-near-white pa4">
-                <Box title="Summary" headingType="inline">
-                    <p className="mt0 measure">Timesheet summary for {summarizedUsers.length} {(this.state.selectedUserIds.length > 0) ? 'selected' : 'displayed'} {(summarizedUsers.length === 1) ? 'employee' : 'employees'}.</p>
+            <div className='bg-near-white pa4'>
+                <Box title='Summary' headingType='inline'>
+                    <p className='mt0 measure'>Timesheet summary for {summarizedUsers.length} {(this.state.selectedUserIds.length > 0) ? 'selected' : 'displayed'} {(summarizedUsers.length === 1) ? 'employee' : 'employees'}.</p>
 
-                    <TimetableSummary className="mt3" users={summarizedUsers}/>
+                    <TimetableSummary className='mt3' users={summarizedUsers}/>
                 </Box>
 
                 <BoxConnector isActive={this.state.nameSearch.length > 0}/>
 
-                <Box title="Filters" headingType="inline">
-                    <label className="db mb2 f6" htmlFor="employees_category">Discrepancy type</label>
+                <Box title='Filters' headingType='inline'>
+                    <label className='db mb2 f6' htmlFor='employees_category'>Discrepancy type</label>
                     <Select
-                        id="employees_category"
-                        name="employees_category"
-                        className="mt0"
-                        options={this.discrepancyTypes}
-                        value={this.state.discrepancyType}
-                        onChange={(discrepancyType) => (discrepancyType !== null) ? this.setState({ discrepancyType: discrepancyType.value }) : this.setState({ discrepancyType: null })}
+                        name='employees_category'
+                        className='mt0'
+                        options={discrepancyTypesOptions}
+                        value={this.state.discrepancyType.label}
+                        onChange={(discrepancyType: DiscrepancyTypes) => (discrepancyType !== null) ? this.setState({ discrepancyType: discrepancyType }) : this.setState({ discrepancyType: null })}
                     />
 
-                    <p className="mt3">
-                        <label className="f6 db" htmlFor="employees_name">Name</label>
-                        <input className="mt2 w-100" type="text" id="employees_name" name="employees_name" value={this.state.nameSearch} onChange={(e) => this.setState({ nameSearch: e.target.value })}/>
+                    <p className='mt3'>
+                        <label className='f6 db' htmlFor='employees_name'>Name</label>
+                        <input className='mt2 w-100' type='text' id='employees_name' name='employees_name' value={this.state.nameSearch} onChange={(e) => this.setState({ nameSearch: e.target.value })}/>
                     </p>
                 </Box>
 
@@ -76,20 +93,9 @@ class TimesheetView extends React.Component {
 
                 <SelectableTimesheetTable
                     users={filteredUsers}
-                    onUpdateSelectedUsers={(selectedUserIds) => this.setState({ selectedUserIds })}
+                    onUpdateSelectedUsers={(selectedUserIds: number[]) => this.setState({ selectedUserIds })}
                 />
             </div>
-        );
+        )
     }
 }
-
-TimesheetView.defaultProps = {
-    className: '',
-};
-
-// TimesheetView.propTypes = {
-//     className: PropTypes.string,
-//     employees: PropTypes.arrayOf(PersonSchema).isRequired,
-// };
-
-export default TimesheetView;
