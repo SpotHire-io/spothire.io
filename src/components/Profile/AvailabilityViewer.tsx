ff252c81a796790c18b248800431c124ef692238
Denis <carriere.denis@gmail.com>
2017-06-26 01:45:43 -0400

    Refactor TS AvailabilityViewer

--- src/components/Profile/AvailabilityViewer.tsx
+++ src/components/Profile/AvailabilityViewer.tsx
@@ -1,23 +1,27 @@
-import 'moment/locale/en-ca';
-import React from 'react';
-import moment from 'moment';
-import classNames from 'classnames';
-import PropTypes from 'prop-types';
-import PersonSchema from '../../schemas/Person';
+import 'moment/locale/en-ca'
+import * as React from 'react'
+import * as moment from 'moment'
+import * as classNames from 'classnames'
+import {Person} from '../../schemas'
 
-const AvailabilityViewer = ({ className, employee }) => {
+interface Props {
+    className?: string
+    employee: Person
+}
+
+const AvailabilityViewer: React.StatelessComponent<Props> = ({ className, employee }) => {
     return (
         <div className={classNames(className)}>
             <ol className="list ma0 pa0 nt2">
                 {
                     Object.keys(employee.availability).map((day, index) => {
-                        const currentDay = employee.availability[day];
+                        const currentDay = employee.availability[day]
 
                         return (
                             <li key={day} className="ma0 ph0 pv2">
                                 <p className="b mt0 mb2">{day.charAt(0).toUpperCase() + day.slice(1)}</p>
                                 {
-                                    (currentDay !== false)
+                                    (currentDay.length)
                                         ? (
                                             <ol className="list pa0">
                                                 {currentDay.map((timeSlot, timeSlotIndex) => (
@@ -29,21 +33,12 @@ const AvailabilityViewer = ({ className, employee }) => {
                                         ) : <p className="i mt0">No availability set.</p>
                                 }
                             </li>
-                        );
+                        )
                     })
                 }
             </ol>
         </div>
-    );
-};
+    )
+}
 
-AvailabilityViewer.defaultProps = {
-    className: '',
-};
-
-// AvailabilityViewer.propTypes = {
-//     className: PropTypes.string,
-//     employee: PersonSchema.isRequired,
-// };
-
-export default AvailabilityViewer;
+export default AvailabilityViewer
