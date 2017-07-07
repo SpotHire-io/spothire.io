66a68c0216d33961a262df1d38c44bf7365f8967
Lucas Cherkewski <lucas@ecustom.ca>
2017-07-07 11:26:58 -0400

    move button type classes to prop

--- src/components/Buttons/BasicButton.tsx
+++ src/components/Buttons/BasicButton.tsx
@@ -6,11 +6,16 @@ interface Props {
     onClick?: React.EventHandler<React.MouseEvent<{}>>
     children: React.ReactNode
     className?: string
+    type?: 'neutral' | 'standard' |  'positive' | 'negative'
 }
 
-const BasicButton: React.StatelessComponent<Props> = ({ children, className='', onClick }) => {
+const BasicButton: React.StatelessComponent<Props> = ({ children, className='', onClick, type }) => {
     let buttonClasses = classNames({
         'button input-reset f5 no-select': true,
+        'button--neutral': type === 'neutral',
+        'button--standard': type === 'standard',
+        'button--positive': type === 'positive',
+        'button--negative': type === 'negative',
         [className]: true
     });
 
