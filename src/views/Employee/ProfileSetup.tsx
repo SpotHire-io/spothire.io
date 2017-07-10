c34c498adf0a1eeea0629f1d4b9519c58bbd8aa8
Lucas Cherkewski <lucas@ecustom.ca>
2017-07-10 10:55:36 -0400

    tweaks to make profile onboarding page more mobile friendly

--- src/views/Employee/ProfileSetup.tsx
+++ src/views/Employee/ProfileSetup.tsx
@@ -22,7 +22,7 @@ export default class ProfileSetup extends React.Component<Props, State> {
         return (
             <div className="bg-near-white pa4">
                 <Box className="mw6 center">
-                    <img src="/img/logo-colour.svg" alt="SpotHire logo" className="db w-50 center mv4"/>
+                    <img src="/img/logo-colour.svg" alt="SpotHire logo" className="db w-90 w-50-ns center mv4"/>
 
                     <p>
                         Welcome to SpotHire! You’ve been invited to join [Organisation’s] SpotHire organisation. Using SpotHire, [Organisation] will
@@ -32,25 +32,25 @@ export default class ProfileSetup extends React.Component<Props, State> {
 
                 <Box className="mt3 mw6 center" title="Account setup" headingType="inline">
                     <form>
-                        <div className="flex mt3">
-                            <div className="w-50 mr4">
+                        <div className="flex-ns mt3">
+                            <div className="w-50-ns mr4-ns">
                                 <label className="f6 db mb2" htmlFor="employee_first_name">First name</label>
                                 <input className="w-100" type="text" id="employee_first_name" name="employee_first_name" defaultValue={this.props.employee.firstName}/>
                             </div>
 
-                            <div className="w-50 mt0">
+                            <div className="mt3 w-50-ns mt0-ns">
                                 <label className="f6 db mb2" htmlFor="employee_last_name">Last name</label>
                                 <input className="w-100" type="text" id="employee_last_name" name="employee_last_name" defaultValue={this.props.employee.lastName}/>
                             </div>
                         </div>
 
-                        <div className="flex mt3">
-                            <div className="w-50 mr4">
+                        <div className="flex-ns mt3">
+                            <div className="w-50-ns mr4-ns">
                                 <label className="f6 db mb2" htmlFor="employee_email">Email address</label>
                                 <input className="w-100" type="email" id="employee_email" name="employee_email" defaultValue={this.props.employee.email}/>
                             </div>
 
-                            <div className="w-50 mt0">
+                            <div className="mt3 w-50-ns mt0-ns">
                                 <label className="f6 db mb2" htmlFor="employee_phone">Phone number</label>
                                 <input className="w-100" type="tel" id="employee_phone" name="employee_phone" defaultValue={this.props.employee.phone}/>
                             </div>
